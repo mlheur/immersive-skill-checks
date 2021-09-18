982b9e35b2d7ad6e7764b93d06ae883278c5bb37
marc <marc_lh@hotmail.com>
2021-09-18 12:48:10 -0700

    feat: read skills from both ruleset and DB

--- scripts/ISC.lua
+++ scripts/ISC.lua
@@ -1,4 +1,5 @@
 ISCnode = "ISC";
+defaultSkills = {"Arcana","History","Insight","Perception","Religion","Stealth","Survival"};
 DEBUG = true;
 function dbg(...) if ISC.DEBUG then print(unpack(arg)) end end
 
@@ -14,44 +15,87 @@ function onInit()
 			0
 		);
 		if DB.getChild(ISCnode,"ISC_aImmersiveSkills") == nil then
-			firstInit();
+			defaultImmersiveSkills();
 		end
 	end
 	ISC.dbg("--ISC:onInit()");
 end
 
-function enumerateSkills()
-	ISC.dbg("++ISC:enumerateSkills()");
-	allSkills = {};
-	for k,v in pairs(DataCommon.psskilldata) do allSkills[v] = 0 end
-	for k,v in pairs(DataCommon.skilldata) do allSkills[k] = 0 end
-	ISC.dbg("--ISC:enumerateSkills()");
-	return allSkills;
+function addSkillNode(skillname,immersive)
+	immersive = immersive or 0;
+	local dbPath = "ISC.ISC_aImmersiveSkills." .. skillname;
+	local dbNode = DB.createNode(dbPath);
+	DB.createNode(dbPath .. ".skillname","string").setValue(skillname);
+	DB.createNode(dbPath .. ".immersive","number").setValue(immersive);
+end
+
+function listDBSkills()
+	ISC.dbg("++ISC:listDBSkills()");
+	local dbSkillNode = DB.findNode("skill");
+	local dbSkillList = {}
+	if dbSkillNode ~= nil then
+		ISC.dbg("  ISC:listDBSkills got skill node");
+		for iSkill,nodeSkill in pairs(dbSkillNode.getChildren()) do
+			for iAttr,attr in pairs(nodeSkill.getChildren()) do
+				if attr.getName() == "name" then
+					dbSkillList[#dbSkillList+1] = attr.getValue();
+					ISC.dbg("  ISC:listDBSkills found child:["..dbSkillList[#dbSkillList].."]");
+				end
+			end
+		end
+	end
+	ISC.dbg("--ISC:listDBSkills()");
+	return dbSkillList;
+end
+
+function listValidSkills()
+	ISC.dbg("++ISC:listValidSkills()");
+	-- hash it out
+	validSkills = {};
+	for skillname in pairs(DataCommon.skilldata, DataCommon.psskilldata) do
+		ISC.dbg("listValidSkills:skillname - ruleset = ["..skillname.."]");
+		validSkills[skillname] = 1;
+	end
+	for i,skillname in pairs(listDBSkills()) do
+		ISC.dbg("listValidSkills:skillname - DB = ["..skillname.."]");
+		validSkills[skillname] = 1;
+	end
+	-- list hash keys
+	validSkillList = {}
+	for skillname in pairs(validSkills) do
+		validSkillList[#validSkillList+1] = skillname;
+	end
+	ISC.dbg("--ISC:listValidSkills()");
+	return validSkillList;
+end
+
+function resetSkills(validSkills)
+	ISC.dbg("++ISC:resetSkills()");
+	validSkills = validSkills or listValidSkills();
+	DB.deleteChildren("ISC.ISC_aImmersiveSkills");
+	for _,skillname in next, listValidSkills() do
+		addSkillNode(skillname,0)
+	end
+	ISC.dbg("--ISC:resetSkills()");
 end
 
 function defaultImmersiveSkills()
 	ISC.dbg("++ISC:defaultImmersiveSkills()");
-	defSkills = enumerateSkills();
-	defSkills["Arcana"]      = 1;
-	defSkills["History"]     = 1;
-	defSkills["Insight"]     = 1;
-	defSkills["Perception"]  = 1;
-	defSkills["Religion"]    = 1;
-	defSkills["Stealth"]     = 1;
-	defSkills["Survival"]    = 1;
+	resetSkills();
+	for _,skillname in pairs(defaultSkills) do
+		addSkillNode(skillname,1)
+	end
 	ISC.dbg("--ISC:defaultImmersiveSkills()");
-	return defSkills;
 end
 
 function loadSkillset(aSkillSet)
 	ISC.dbg("++ISC:loadSkillset()");
-	DB.deleteChildren("ISC.ISC_aImmersiveSkills");
-	for k,v in pairs(aSkillSet) do
-		ISC.dbg("ISC:loadSkillset() given [k:["..tostring(k).."],v:["..tostring(v).."]]");
-		local dbPath = "ISC.ISC_aImmersiveSkills." .. k;
-		local dbNode = DB.createNode(dbPath);
-		DB.createNode(dbPath .. ".immersive","number").setValue(v);
-		DB.createNode(dbPath .. ".skillname","string").setValue(k);
+	validSkills = listValidSkills();
+	resetSkills(validSkills);
+	for skillname,immersive in pairs(aSkillSet) do
+		if validSkills[skillname] ~= nil then
+			addSkillNode(skillname,immersive);
+		end
 	end
 	ISC.dbg("--ISC:loadSkillset()");
 end
@@ -72,10 +116,4 @@ function getSkillset()
 	end
 	ISC.dbg("--ISC:getSkillset()");
 	return aSkillSet;
-end
-
-function firstInit()
-	ISC.dbg("++ISC:firstInit()");
-	loadSkillset(defaultImmersiveSkills());
-	ISC.dbg("--ISC:firstInit()");
 end
