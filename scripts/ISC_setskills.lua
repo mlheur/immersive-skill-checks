ff0f9ceb40eac211802eb6fc7e4adc427e764358
marc <marc_lh@hotmail.com>
2021-09-23 14:49:23 -0700

    fix: cleanup after reverting failed introduction of templates

--- scripts/ISC_setskills.lua
+++ scripts/ISC_setskills.lua
@@ -5,12 +5,13 @@ function onInit()
 	self["ISC_button_ok"].onButtonPress = acceptInput;
 	self["ISC_button_defaults"].onButtonPress = resetDefaults;
 	-- ToDo: add a handler to redo ISC.loadskillset() on DB change.
+	-- ToDo: add a hook to open the skill window from the setskills window.
 	aSavedImmersiveSkills = ISC.getSkillset();
 	ISC.loadSkillset(aSavedImmersiveSkills);
 end
 
 function resetDefaults()
-	ISC.loadSkillset(ISC.defaultImmersiveSkills());
+	ISC.defaultImmersiveSkills()
 end
 
 function revertInput()
