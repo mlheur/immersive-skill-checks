10b81c0d7430bad2d148931830b5bd3cb412ff91
marc <marc_lh@hotmail.com>
2021-12-12 08:17:25 -0800

    feat: much refactoring, and it works

--- scripts/ISC.lua
+++ scripts/ISC.lua
@@ -1,8 +1,10 @@
-ISCpath = "ISC";
-RESULTS = ISCpath..".ISC_aResults";
-SKILLS = ISCpath..".ISC_aImmersiveSkills";
-defaultSkills = {"Arcana","History","Insight","Perception","Religion","Stealth","Survival"};
 DEBUG = true;
+
+DBPATH = "ISC";
+RESULTS = DBPATH..".roll-data";
+SKILLS = DBPATH..".immersive-skills";
+TITLES = DBPATH..".skill-titles"
+DEFAULTS = {"Arcana","History","Insight","Perception","Religion","Stealth","Survival"};
 
 function dbg(...) if ISC.DEBUG then print(unpack(arg)) end end
 
@@ -13,125 +15,26 @@ function onInit()
 			"ISC_button_up",
 			"ISC_button_dn",
 			"Immersion",
-			"ISC_results_window",
-			"ISC",
+			"ISC_resultswindow",
+			ISC.DBPATH,
 			0
 		);
-		if DB.getChild(SKILLS) == nil then
-			defaultImmersiveSkills();
+		if DB.getChild(ISC.SKILLS) == nil then
+			ISC_SkillsMgr.setDefaultImmersiveSkills();
 		end
+		ActionsManager.registerResultHandler("immersive-skill-check", ISC_ResultsMgr.onRoll)
+		ISC_ResultsMgr.openResults()
 	end
 	ISC.dbg("--ISC:onInit()");
 end
 
-function resetResults()
-	DB.deleteChildren(RESULTS)
-end
-
-function addCombatant(keyCT, nodeCT)
-	local pathCT = nodeCT.getPath()
-	local name = DB.getValue(pathCT .. ".name")
-	local pcnpc = DB.getValue(pathCT .. ".link", "class")
-	local dbPath = RESULTS .. "." .. keyCT
-	local dbNode = DB.createNode(dbPath)
-	DB.createNode(dbPath .. ".ISC_charname", "string").setValue(name)
-	DB.createNode(dbPath .. ".ISC_chartype", "string").setValue(pcnpc)
-	dbg("==ISC:addCombatant(ISC_charname=["..name.."],ISC_chartype=["..pcnpc.."])")
-end
-
-function addSkillNode(skillname,immersive)
-	immersive = immersive or 0;
-	local dbPath = SKILLS .. "." .. skillname;
-	local dbNode = DB.createNode(dbPath);
-	DB.createNode(dbPath .. ".skillname","string").setValue(skillname);
-	DB.createNode(dbPath .. ".immersive","number").setValue(immersive);
-end
-
-function listDBSkills()
-	ISC.dbg("++ISC:listDBSkills()");
-	local dbSkillNode = DB.findNode("skill");
-	local dbSkillList = {}
-	if dbSkillNode ~= nil then
-		ISC.dbg("  ISC:listDBSkills got skill node");
-		for iSkill,nodeSkill in pairs(dbSkillNode.getChildren()) do
-			for iAttr,attr in pairs(nodeSkill.getChildren()) do
-				if attr.getName() == "name" then
-					dbSkillList[#dbSkillList+1] = attr.getValue();
-					ISC.dbg("  ISC:listDBSkills found child:["..dbSkillList[#dbSkillList].."]");
-				end
-			end
+function findWindow(windowlist,dbPath)
+	ISC.dbg("==ISC:findWindow(): dbPath=["..dbPath.."]")
+	for _,w in pairs(windowlist.getWindows()) do
+		if w.getDatabaseNode().getPath() == dbPath then
+			return w
 		end
 	end
-	ISC.dbg("--ISC:listDBSkills()");
-	return dbSkillList;
+	return nil
 end
 
-function listValidSkills()
-	ISC.dbg("++ISC:listValidSkills()");
-	-- hash it out
-	validSkills = {};
-	for skillname in pairs(DataCommon.skilldata, DataCommon.psskilldata) do
-		ISC.dbg("listValidSkills:skillname - ruleset = ["..skillname.."]");
-		validSkills[skillname] = 1;
-	end
-	for i,skillname in pairs(listDBSkills()) do
-		ISC.dbg("listValidSkills:skillname - DB = ["..skillname.."]");
-		validSkills[skillname] = 1;
-	end
-	-- list hash keys
-	validSkillList = {}
-	for skillname in pairs(validSkills) do
-		validSkillList[#validSkillList+1] = skillname;
-	end
-	ISC.dbg("--ISC:listValidSkills()");
-	return validSkillList;
-end
-
-function resetSkills(validSkills)
-	ISC.dbg("++ISC:resetSkills()");
-	validSkills = validSkills or listValidSkills();
-	DB.deleteChildren(SKILLS);
-	for _,skillname in next, listValidSkills() do
-		addSkillNode(skillname,0)
-	end
-	ISC.dbg("--ISC:resetSkills()");
-end
-
-function defaultImmersiveSkills()
-	ISC.dbg("++ISC:defaultImmersiveSkills()");
-	resetSkills();
-	for _,skillname in pairs(defaultSkills) do
-		addSkillNode(skillname,1)
-	end
-	ISC.dbg("--ISC:defaultImmersiveSkills()");
-end
-
-function loadSkillset(aSkillSet)
-	ISC.dbg("++ISC:loadSkillset()");
-	validSkills = listValidSkills();
-	resetSkills(validSkills);
-	for skillname,immersive in pairs(aSkillSet) do
-		if validSkills[skillname] ~= nil then
-			addSkillNode(skillname,immersive);
-		end
-	end
-	ISC.dbg("--ISC:loadSkillset()");
-end
-
-function getSkillset()
-	ISC.dbg("++ISC:getSkillset()");
-	local aSkillSet = {};
-	for iSkill,skillNode in pairs(DB.getChildren(SKILLS)) do
-		local immersive = 0;
-		local skillname = "**unset**"
-		for iAttr,attr in pairs(skillNode.getChildren()) do
-			attrName = attr.getName();
-			if attrName == "immersive" then immersive = attr.getValue() end
-			if attrName == "skillname" then skillname = attr.getValue() end
-		end
-		aSkillSet[skillname] = immersive;
-		ISC.dbg("  ISC:getSkillset() saving skill [k:["..tostring(skillname).."],v:["..tostring(immersive).."]]")
-	end
-	ISC.dbg("--ISC:getSkillset()");
-	return aSkillSet;
-end
