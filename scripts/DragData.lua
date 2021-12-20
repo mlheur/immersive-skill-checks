035399f3a46654322fe17b17c238e5430d3d293c
marc <marc_lh@hotmail.com>
2021-12-20 09:44:13 -0800

    refactor: rewrote most of it after better understanding Ruleset and APIs

---
+++ scripts/DragData.lua
@@ -0,0 +1,240 @@
+-- https://www.fantasygrounds.com/forums/showthread.php?48145-Creating-Cloning-or-Writing-a-Class-for-dragdata
+-- https://www.fantasygrounds.com/forums/showthread.php?50651-Custom-Draginfo
+
+--When drag objects are set using the setData function, here is the expected format of the LUA table used to define the drag object:
+--
+--    type: String. Drag object type.
+--    icon: String. Icon associated with this drag object.
+--    description: String. Description associated with this drag object.
+--    dbref: String (or databasenode). Data base node identifier (or databasenode object) associated with this drag object.
+--    secret: Boolean. Flag indicating whether drag data should be displayed local only when dropped on chat window.
+--    shortcuts: Table of shortcut records. Each shortcut record is a table containing class and recordname string fields. Any shortcut records associated with this drag object.
+--    slots: Table of slot records. See below.
+--
+--    Each slot record is a LUA table with the following fields:
+--    
+--    type: String. Slot type.
+--    number: Number. Numeric value associated with this slot.
+--    string: String. Textual value associated with this slot.
+--    token: String. Token identified associated with this slot.
+--    shortcut: Table. Shortcut associated with this slot. Each shortcut record is a table containing class and recordname string fields.
+--    dice: Table. Dice associated with this slot. The table contains numerically indexed die resource names.
+--    metadata: Table. Metadata associated with this slot.
+--    custom: Object. Custom LUA variable associated with this slot. This information will not be saved for drag objects placed on hot key bar.
+
+__DEBUG__ = true
+function dbg(...) if __DEBUG__ then print("[DragData]:"..unpack(arg)) end end
+
+dd = {}
+
+function ddSlot()
+    dbg("++ddSlot()")
+    s = {}
+    s.type = nil
+    s.number = nil
+    s.string = nil
+    s.token = nil
+    s.shortcut = {} -- s.shortcut[1].class = "wndclass"; s.shortcut[1].recordname = rec
+    s.dice = {} -- s.dice[1] = "d20"
+    s.metadata = {} -- s.metadata[k] = v
+    s.custom = nil
+    dbg("--ddSlot()")
+    return s
+end
+
+function createBaseData()
+    dbg("++createBaseData()")
+    -- members
+    dd.type = nil
+    dd.icon = nil
+    dd.description = nil
+    dd.dbref = nil
+    dd.secret = false
+    dd.shortcuts = {}
+    dd.slots = {ddSlot(type)}
+    dd.slot_ptr = 1
+    dd.hotkey_enabled = false
+    dd.reveal = false
+    -- functions
+    dd.addDie = addDie
+	dd.addShortcut = addShortcut
+	dd.createBaseData = createBaseData
+	dd.disableHotkeying = disableHotkeying
+	dd.getCustomData = getCustomData
+	dd.getDatabaseNode = getDatabaseNode
+	dd.getDescription = getDescription
+	dd.getDieList = getDieList
+	dd.getMetaData = getMetaData
+	dd.getMetaDataList = getMetaDataList
+	dd.getNumberData = getNumberData
+	dd.getSecret = getSecret
+	dd.getShortcutData = getShortcutData
+	dd.getShortcutList = getShortcutList
+	dd.getSlot = getSlot
+	dd.getSlotCount = getSlotCount
+	dd.getSlotType = getSlotType
+	dd.getStringData = getStringData
+	dd.getTokenData = getTokenData
+	dd.getType = getType
+	dd.isType = isType
+	dd.nextSlot = nextSlot
+	dd.reset = reset
+	dd.resetType = resetType
+	dd.revealDice = revealDice
+	dd.setCustomData = setCustomData
+	dd.setData = setData
+	dd.setDatabaseNode = setDatabaseNode
+	dd.setDatabaseNode = setDatabaseNode
+	dd.setDescription = setDescription
+	dd.setDieList = setDieList
+	dd.setIcon = setIcon
+	dd.setMetaData = setMetaData
+	dd.setNumberData = setNumberData
+	dd.setSecret = setSecret
+	dd.setShortcutData = setShortcutData
+	dd.setSlot = setSlot
+	dd.setSlotType = setSlotType
+	dd.setStringData = setStringData
+	dd.setTokenData = setTokenData
+	dd.setType = setType
+    dbg("--createBaseData()")
+    return dd
+end
+
+function addDie(d)
+    dbg("+-addDie("..d..")")
+    table.insert(dd.slots[dd.slot_ptr].dice, d)
+end
+
+function addShortcut(c,r)
+    dbg("+-addShortcut("..c..","..r..")")
+    s = {}
+    s.class = c
+    s.record = r
+    table.insert(dd.shortcuts, s)
+end
+
+function disableHotkeying(s)
+    dbg("+-disableHotkeying("..s..")")
+    s = s or false
+    dd.hotkey_enabled = s
+end
+
+function getCustomData() return dd.slots[dd.slot_ptr].custom end
+function getDatabaseNode() return dd.dbref end
+function getDescription() return dd.description end
+function getDieList() return dd.slots[dd.slot_ptr].dice end
+function getMetaData(k) return dd.slots[dd.slot_ptr].metadata[k] end
+function getMetaDataList() return dd.slots[dd.slot_ptr].metadata end
+function getNumberData() return dd.slots[dd.slot_ptr].number end
+function getSecret() return dd.secret end
+function getShortcutData() return dd.slots[dd.slot_ptr].shortcut.class, dd.slots[dd.slot_ptr].shortcut.recordname end
+function getShortcutList() return dd.slots[dd.slot_ptr].shortcut end
+function getSlot() return dd.slot_ptr end
+function getSlotCount() return #dd.slots end
+function getSlotType() return dd.slots[dd.slot_ptr].type end
+function getStringData() return dd.slots[dd.slot_ptr].string end
+function getTokenData() return dd.slots[dd.slot_ptr].token end
+function getType() return dd.type end
+function isType(sCmp) return dd.type == sCmp end
+function nextSlot() dd.slot_ptr = dd.slot_ptr + 1 end
+function reset()
+    dd = createBaseData()
+end
+function resetType() dd.type = dd.slots[dd.slot_ptr].type end
+function revealDice(r) dd.reveal = r end
+function setCustomData(c) dd.slots[dd.slot_ptr].custom = c end
+
+function setData(dr)
+    dbg("+-setData()")
+    dd.type = dr.type
+	dd.icon = dr.icon
+	dd.description = dr.description
+	dd.dbref = dr.dbref
+	dd.secret = dr.secret
+	dd.shortcuts = dr.shortcuts
+	dd.slots = dr.slots
+	dd.slot_ptr = dr.slot_ptr
+	dd.hotkey_enabled = dr.hotkey_enabled
+	dd.reveal = dr.reveal
+end
+
+function setDatabaseNode(n)
+    if type(n) ~= "string" then
+        n = n.getPath()
+    end
+    dbg("+-setDatabaseNode("..n..")")
+    dd.dbref = n
+end
+
+function setDescription(d)
+    dbg("+-setDescription("..d..")")
+    dd.description = d
+end
+
+function setDieList(d)
+    dbg("+-setDieList()")
+    dd.slots[dd.slot_ptr]["dice"] = d
+end
+
+function setIcon(i)
+    dbg("+-setIcon("..i..")")
+    dd.icon = i
+end
+
+function setMetaData(k,v)
+    dbg("+-setMetaData("..k..","..v..")")
+    dd.slots[dd.slot_ptr]["metadata"][k] = v
+end
+
+function setNumberData(n)
+    dbg("+-setNumberData("..n..")")
+    dd.slots[dd.slot_ptr]["number"] = n
+end
+
+function setSecret(s)
+    if s then dbg("+-setSecret(true)")
+    else dbg("+-setSecret(false)")
+    end
+    dd.secret = s
+end
+
+function setShortcutData(c,r)
+    dbg("+-setShortcutData("..c..","..r..")")
+    s = {}
+    s.class = c
+    s.recordname = r
+    dd.slots[dd.slot_ptr].shortcut = s
+end
+
+function setSlot(i)
+    i = i or 1
+    oMax = #dd.slots
+    dbg("+-setSlot("..i..") oMax["..oMax.."]")
+    dd.slot_ptr = i
+    i = i + 1
+    while i <= oMax do
+        dd.slots[i] = nil
+        i = i + 1
+    end
+end
+
+function setSlotType(t)
+    dbg("+-setSlotType("..t..")")
+    dd.slots[dd.slot_ptr]["type"] = t
+end
+
+function setStringData(s)
+    dbg("+-setStringData("..s..")")
+    dd.slots[dd.slot_ptr]["string"] = s
+ end
+
+ function setTokenData(t)
+    dbg("+-setTokenData("..t..")")
+    dd.token = t
+end
+
+function setType(t)
+    dbg("+-setType("..t..")")
+    dd.type = t
+end
---
+++ scripts/iscThrowMgr.lua
@@ -0,0 +1,126 @@
+--------------------------------------------------------------------------------
+-- PRE Callback
+--------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------
+function doRoundChange()
+	ISC.dbg("++iscThrowMgr:doRoundChange()")    
+	bAutoroll = DB.getValue("ISC.bAutoroll")
+	if bAutoroll ~= nil and bAutoroll ~= 0 then
+        wResults = Interface.findWindow("immersive_results","ISC")
+        if wResults then
+            wResults["rollnow"].onButtonPress()
+        else
+            DB.createChild("ISC", "bPendingRoll", "number").setValue(1)
+        end
+	end
+	ISC.dbg("--iscThrowMgr:doRoundChange()")    
+end
+
+--------------------------------------------------------------------------------
+function mkRoll(nResult)
+	ISC.dbg("++iscThrowMgr:mkRoll()")    
+	local r = {}
+	r["aDice"]   = { "d20" } -- lookup? put a d20 in the XML?
+	r["nMod"]    = 0 -- will be set after resolving actor type and looking up their ability
+	r["sType"]   = "ISC" -- must match ActionsManager.registerResultHandler("ISC", ...) for our callback to be called
+	r["sDesc"]   = "" -- draginfo.getStringData(), will be set after draginfo is set by ActionSkill.perform*Rolld
+	r["bSecret"] = true -- these are immersive, never show the players.
+	r["pResult"] = nResult.getPath() -- this gets passed on blindly to our onRoll function
+	ISC.dbg("--iscThrowMgr:mkRoll()")    
+	return r
+end
+
+--------------------------------------------------------------------------------
+function throwDice(nResult)
+	ISC.dbg("++iscThrowMgr:throwDice()")    
+	if nResult == nil then
+		ISC.dbg("--iscThrowMgr:throwDice(): got nil, bailing")
+		return
+	end
+	local rActor = ActorManager.resolveActor(nResult.getParent().getParent())
+	local rThrow = ActionsManager.buildThrow(rActor, {}, mkRoll(nResult), false);
+    nResult.getChild("total").setValue(99)
+    nResult.getChild("label").setValue("[**]")
+	Comm.throwDice(rThrow);
+	ISC.dbg("--iscThrowMgr:throwDice()")    
+end
+
+--------------------------------------------------------------------------------
+-- POST Callback
+--------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------
+-- beautify the modifier for display above the result.
+function mkBonus(nMod)
+	s = ""
+	if nMod >= 0 then s = "+" end
+	return s..nMod
+end
+
+--------------------------------------------------------------------------------
+function getCharsheetSkill(pCharsheet, sSkill)
+	ISC.dbg("++iscThrowMgr:getCharsheetSkill(): pCharsheet=["..pCharsheet.."], sSkill=["..sSkill.."]")
+    nCharsheet = DB.findNode(pCharsheet)
+    if nCharsheet == nil then return nil end
+    nSkilllist = nCharsheet.getChild("skilllist")
+    if nSkilllist == nil then return nil end
+    for key,nCharsheetSkill in pairs(nSkilllist.getChildren()) do
+        if nCharsheetSkill.getChild("name").getValue() == sSkill then return nCharsheetSkill end
+    end
+	ISC.dbg("--iscThrowMgr:getCharsheetSkill(): return nil out the bottom")
+    return nil
+end
+
+--------------------------------------------------------------------------------
+function getPCBonus(rActor, sSkill)
+    if rActor["sType"] ~= "charsheet" then return nil end
+    nCharSkill = getCharsheetSkill(rActor["sCreatureNode"],sSkill)
+    if nCharSkill == nil then return nil end
+    draginfo = DragData.createBaseData()
+    ActionSkill.performRoll(draginfo, rActor, nCharSkill)
+    return draginfo.getNumberData()
+end
+
+--------------------------------------------------------------------------------
+function getSkillAbility(sSkill)
+	ISC.dbg("++iscThrowMgr:getSkillAbility(): sSkill=["..sSkill.."]")
+    -- run the local list first, override DataCommon if there's one with the same name
+    for kSkill,nSkill in pairs(DB.getChildren(ISC.dbskill_data)) do
+        if nSkill.getChild("name").getValue() == sSkill then
+            nStat = nSkill.getChild("stat")
+            if nStat == nil then
+                ISC.dbg("--iscThrowMgr:getSkillAbility(): from ISC.dbskill_data return 0, skill has no stat block")
+                return 0
+            end
+        	ISC.dbg("--iscThrowMgr:getSkillAbility(): from ISC.dbskill_data return nStat.getValue():lower()")
+            return nStat.getValue():lower()
+        end
+    end
+    for sName,tSkill in pairs(DataCommon.skilldata) do
+        if sName == sSkill then
+        	ISC.dbg("--iscThrowMgr:getSkillAbility(): from DataCommon.skilldata return tSkill[stat]:lower()")
+            return tSkill["stat"]:lower()
+        end
+    end
+    ISC.dbg("--iscThrowMgr:getSkillAbility(): return nil out the bottom")
+end
+
+--------------------------------------------------------------------------------
+-- this is an asyncronous callback from FGU after the dice have been thrown.
+-- everything we need for displaying the result has to be passed to us in
+-- one of the three parameters.  rRoll is the best structure to use for this,
+-- the throw mechanics will try to pass along unsupported string attributes as
+-- faithfully as possible, KISS works well.
+function onRoll(rSource, rTarget, rRoll)
+    local sSkill = DB.findNode(rRoll.pResult).getChild("name").getValue()
+    local nBonus = getPCBonus(rSource, sSkill) or (ActorManager5E.getAbilityBonus(rSource, getSkillAbility(sSkill)) or 0)
+
+    ActionsManager2.decodeAdvantage(rRoll); -- check and update ADV/DIS modifiers in rRoll struct, not sure if it catches all of them or what?
+    local nTotal = ActionsManager.total(rRoll); -- Apply modifiers and effects to the pure d20 result, and return the modified total.
+
+	ISC.dbg("==ISC_resultsmanager:onRoll() sSkill=["..sSkill.."], nBonus=["..nBonus.."], nTotal=["..nTotal.."]")
+	nResult = DB.findNode(rRoll.pResult)
+	nResult.getChild("label").setValue(mkBonus(nBonus))
+	nResult.getChild("total").setValue(nTotal)
+end
